9367e3e6b338b155b8c1da122ee7aadc7e143d4f
lukelong <lukelongdad@gmail.com>
2020-07-20 14:56:42 +0800

    init project, add package, pom and some dependencies

---
+++ src/main/java/com/luke/luke_blog/LukeBlogApplication.java
@@ -0,0 +1,19 @@
+package com.luke.luke_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * @author zhang
+ * @date 2020/07/20
+ */
+@SpringBootApplication
+public class LukeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LukeBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luke/luke_blog/LukeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luke.luke_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukeBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
