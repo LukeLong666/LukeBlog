1b31131e9bd09a706364188e8ed64ee55963820c
lukelong <lukelongdad@gmail.com>
2020-08-03 09:31:00 +0800

    n

--- src/main/java/com/luke/luke_blog/controller/ErrorPageController.java
+++ src/main/java/com/luke/luke_blog/controller/ErrorPageController.java
@@ -1,7 +1,7 @@
 package com.luke.luke_blog.controller;
 
 import com.luke.luke_blog.response.ResponseResult;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,27 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ErrorPageController {
 
-    @GetMapping("/403")
+    @RequestMapping("/403")
     public ResponseResult page403() {
         return ResponseResult.FAILURE(40003, "权限不足", null);
     }
 
-    @GetMapping("/404")
+    @RequestMapping("/404")
     public ResponseResult page404() {
         return ResponseResult.FAILURE(40004, "页面丢失", null);
     }
 
-    @GetMapping("/500")
+    @RequestMapping("/500")
     public ResponseResult page500() {
         return ResponseResult.FAILURE(50004, "系统繁忙,请稍后重试", null);
     }
 
-    @GetMapping("/504")
+    @RequestMapping("/504")
     public ResponseResult page504() {
         return ResponseResult.FAILURE(50004, "系统繁忙,请稍后重试", null);
     }
 
-    @GetMapping("/505")
+    @RequestMapping("/505")
     public ResponseResult page505() {
         return ResponseResult.FAILURE(50005, "请求错误,请检查参数", null);
     }
