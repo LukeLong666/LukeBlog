41d7d549761c29b11896178ec508892c555d5ba4
张鑫龙 <lukelong666@163.com>
2021-06-09 16:48:58 +0800

    update TextUtils.java 添加手机号正则验证

--- src/main/java/com/luke/luke_blog/utils/TextUtils.java
+++ src/main/java/com/luke/luke_blog/utils/TextUtils.java
@@ -3,17 +3,28 @@ package com.luke.luke_blog.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *
+ * @author zxl
+ * @date 2021/06/09
+ */
 public class TextUtils {
-
-    public static final String regEx = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 
     public static boolean isEmpty(String text) {
         return text==null||text.length()==0;
     }
 
     public static boolean isEmailAddress(String emailAddress) {
+        String regEx = "/^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$/";
         Pattern p = Pattern.compile(regEx);
         Matcher m = p.matcher(emailAddress);
+        return m.matches();
+    }
+
+    public static boolean isPhoneNumber(String phoneNumber) {
+        String regEx = "/^1(?:3\\d|4[4-9]|5[0-35-9]|6[67]|7[013-8]|8\\d|9\\d)\\d{8}$/";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(phoneNumber);
         return m.matches();
     }
 }
